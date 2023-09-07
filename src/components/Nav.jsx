d324e214bf37375ca4d0225a224f4ef2b6cc46f2
Sai Aryan Goswami <saiaryan.goswami1784@gmail.com>
2023-09-08 00:09:17 +0530

    change made

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='p-5 text-white'>
+    <div className='p-5 text-white w-50px h-48px'>
         <div className='flex justify-between'>
-            <div className='w-[50%]'>
-                Creators Campus Connect
+        <img src="src\assets\image 1.png" alt="" className='w-50px h-48px'/>
+            <div className='w-[50%] m-auto ml-4'>
+                Creators Campus <br /> Connect
             </div>
             <ul className='flex justify-evenly w-[50%]'>
-                <li>Event</li>
-                <li>Speakers</li>
-                <li>Schedule</li>
+                <img src="src\assets\Storefront.png" alt="" className='w-50px h-50px'/>
+                <li className='my-auto'>Event</li>
+                <img src="src\assets\HandWaving.png" alt="" className='w-50px h-50px'/>
+                <li className='my-auto'>Speakers</li>
+                <img src="src\assets\CalendarBlank.png" alt="" className='w-50px h-50px'/>
+                <li className='my-auto'>Schedule</li>
             </ul>
         </div>
     </div>
